1a98e17335bb00a46f21c51c53e9f66f8c998c30
PAYAL_PATEL <PAYAL89>
2022-12-28 22:02:08 +0530

    Updates

---
+++ precache-manifest.903a926b03b3ae47f3e1259b5351f1d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "382598b07f9c9b299a5c",
+    "url": "/MyShopping/static/css/main.1b289318.chunk.css"
+  },
+  {
+    "revision": "382598b07f9c9b299a5c",
+    "url": "/MyShopping/static/js/main.382598b0.chunk.js"
+  },
+  {
+    "revision": "128fd6128b9459c5ab86",
+    "url": "/MyShopping/static/js/1.128fd612.chunk.js"
+  },
+  {
+    "revision": "c97e3e75f142e14f9a85",
+    "url": "/MyShopping/static/css/2.00e146bc.chunk.css"
+  },
+  {
+    "revision": "c97e3e75f142e14f9a85",
+    "url": "/MyShopping/static/js/2.c97e3e75.chunk.js"
+  },
+  {
+    "revision": "7cfefbeda47f1d22f6b5",
+    "url": "/MyShopping/static/js/runtime~main.7cfefbed.js"
+  },
+  {
+    "revision": "0e940107039d88db507ffbc99a5f7f5c",
+    "url": "/MyShopping/index.html"
+  }
+];
